e7decca51c4dc34cd1c16cd260b9665d675336dc
樊先知樊先知 <2360316295@qq.com>
2018-04-25 17:12:03 +0800

    更新数据库脚本以及相关操作数据库的java代码

--- warehouse-web/src/main/resources/sql/warehouse.sql
+++ warehouse-web/src/main/resources/sql/warehouse.sql
@@ -2,12 +2,12 @@ CREATE DATABASE warehouse CHARACTER SET utf8;
 USE warehouse;
 
 #人员表
-CREATE TABLE `user` (
+CREATE TABLE `userinfo` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表主键',
-  `username` varchar(255) NOT NULL COMMENT '用户名',
-  `character` int(4) NOT NULL DEFAULT '3' COMMENT '权限，1是超级管理员，2是普通管理员，3是普通用户',
-  `password` varchar(255) NOT NULL COMMENT '密码',
-  `remark` varchar(255) NOT NULL COMMENT '备注',
+  `username` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
+  `level` int(4) NOT NULL DEFAULT '3' COMMENT '权限，1是超级管理员，2是普通管理员，3是普通用户',
+  `password` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
+  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建的时间',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新的时间',
   PRIMARY KEY (`id`)
@@ -18,15 +18,15 @@ CREATE TABLE `user` (
 CREATE TABLE `goods` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表主键',
   `num` varchar(255) NOT NULL DEFAULT '' COMMENT '物资编号',
-  `name` varchar(100) NOT NULL COMMENT '物资名称',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '物资名称',
   `extends_parts` varchar(255) NOT NULL DEFAULT '' COMMENT '扩展配件',
   `price` double NOT NULL DEFAULT '0' COMMENT '商品的单价',
-  `remark` varchar(255) NOT NULL COMMENT '物资备注',
+  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '物资备注',
   `operator` varchar(70) NOT NULL DEFAULT '' COMMENT '操作人',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建日期',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新记录的日期',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='物资信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='物资信息表' ;
 
 #仓储表
 CREATE TABLE `warehouse` (
@@ -36,7 +36,7 @@ CREATE TABLE `warehouse` (
   `responser` varchar(50) NOT NULL DEFAULT '' COMMENT '仓库负责人',
   `responser_phone` varchar(70) NOT NULL DEFAULT '' COMMENT '仓库负责人联系方式',
   `company` varchar(150) NOT NULL DEFAULT '' COMMENT '仓库所属公司',
-  `contract_date` datetime NOT NULL COMMENT '仓库签订合同的日期',
+  `contract_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '仓库签订合同的日期',
   `operator` varchar(70) NOT NULL DEFAULT '' COMMENT '操作人',
   `updateed_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日期',
   `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
@@ -48,11 +48,11 @@ CREATE TABLE `supplier` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表主键',
   `num` varchar(255) NOT NULL DEFAULT '' COMMENT '供应商编号',
   `name` varchar(100) NOT NULL DEFAULT '' COMMENT '供应商名称',
-  `receiver` varchar(50) NOT NULL COMMENT '对接人',
+  `receiver` varchar(50) NOT NULL DEFAULT '' COMMENT '对接人',
   `receiver_phone` varchar(70) NOT NULL DEFAULT '' COMMENT '对接人联系方式',
-  `responser` varchar(50) NOT NULL COMMENT '负责人',
+  `responser` varchar(50) NOT NULL DEFAULT '' COMMENT '负责人',
   `responser_phone` varchar(70) NOT NULL DEFAULT '' COMMENT '负责人联系方式',
-  `operator` varchar(70) NOT NULL COMMENT '操作人',
+  `operator` varchar(70) NOT NULL DEFAULT '' COMMENT '操作人',
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updatetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
@@ -79,6 +79,6 @@ CREATE TABLE `log` (
   `operator_num` varchar(255) NOT NULL DEFAULT '' COMMENT '操作者编号',
   `opreator_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '操作时间',
   `operation_type` varchar(50) NOT NULL DEFAULT '' COMMENT '操作类型，增加、删除、修改',
-  `operation_detail` varchar(255) NOT NULL COMMENT '操作商品细节',
+  `operation_detail` varchar(255) NOT NULL DEFAULT '' COMMENT '操作商品细节',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='操作日志表';
