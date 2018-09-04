7d8e59a94e00dbf20271c80f6c0b7f7261d454de
XIANZHIXIANZHIXIAN <2360316295@qq.com>
2018-09-04 22:01:52 +0800

    修改到IDEA使用,添加tomcat7插件

--- warehouse-utils/src/main/java/com/warehouse/utils/Test.java
+++ warehouse-utils/src/main/java/com/warehouse/utils/Test.java
@@ -3,7 +3,7 @@ package com.warehouse.utils;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(MD5.getMD5("123456"));
 	}
 
 }
